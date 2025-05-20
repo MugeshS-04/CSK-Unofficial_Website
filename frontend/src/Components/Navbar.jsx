258582fdea7e1d962048d47cefdd940ca6960aec
MUGESH SELVARAJ <mukeshselvaraj004@gmail.com>
2025-05-20 19:19:29 +0530

    Imported font and changed in Navbar

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <div className='w-[90%] ml-[4%] mt-[4%] flex items-center gap-1'>
         <img className='w-[5%] h-auto' src={logo} alt="CSK Logo" />
-        <h1 className='ml-4 text-3xl font-bold'>Chennai Super Kings</h1> {/* Added left margin */}
+        <h1 className='inria-sans ml-4 text-3xl'>Chennai Super Kings</h1>
     </div>
   )
 }
